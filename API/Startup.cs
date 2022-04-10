f7f1a72789db4116a41c789e78585bf3f6881ef4
igalovic71 <igalovic@foi.hr>
2022-04-10 18:02:20 +0200

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,20 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Core.Interfaces;
 using Infrastructure.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using API.Helpers;
 using AutoMapper;
+using API.Middleware;
+using API.Extensions;
 
 namespace API
 {
@@ -30,20 +19,24 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) //add to service so we can use it in other parts of application
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),(typeof(GenericRepository<>)));
+
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection"))); //GetConnectionString - Shorthand for GetSection("ConnectionStrings")[name]. Drzi context aktivnom dok god je aktivan http request s atributom scope
+
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseSwaggerDocumentation();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}"); //in the event that request comes into our API server, but we do not have an end point that matches that particular request, then we are going to hit this bit of middleware and it's going to redirect to our errors controller and pass in the status code and in our errors controller 
 
             app.UseHttpsRedirection();
 
