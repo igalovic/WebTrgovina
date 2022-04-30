02d716ab3960369faf6cfbfd33f9f837d6a9ef98
igalovic71 <igalovic@foi.hr>
2022-04-30 11:45:59 +0200

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 /* Decorator koji se identificira kao angular component */
 @Component({
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Web trgovina';
+  products:IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    /*observable and we need to subscribe to it */
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response:IPagination) =>{
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
