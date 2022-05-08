622b958e06cb24613cf26c20aa3ec2d0712d633c
igalovic71 <igalovic@foi.hr>
2022-05-08 10:30:19 +0200

    End of section 10

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
-
+const routes: Routes = [
+  {path: '', component: ShopComponent},
+  {path: ':id',component:ProductDetailsComponent},
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    /* forChild- routes are only available for our shop module(not available in app.module.ts) */
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
 })
 export class ShopRoutingModule { }
